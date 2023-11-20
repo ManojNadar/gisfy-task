626bbf5947fadf89fbe953fb561da62dedda441f
ManojNadar <manojviru03@gmail.com>
2023-11-20 12:33:29 +0530

    update

---
+++ gisfytask/App.js
@@ -0,0 +1,10 @@
+import { StyleSheet, View } from "react-native";
+import Main from "./Components/Main";
+
+export default function App() {
+  return (
+    <View>
+      <Main />
+    </View>
+  );
+}
